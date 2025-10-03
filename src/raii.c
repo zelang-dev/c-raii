aea0a124f8530abf24d25ca4bb41af91081a3c37
Lawrence Stubbs <lstubbs@techno.express>
2025-10-02 22:53:11 -0400

    revert commit/tag 2.2.2 conflicting macro/function

--- src/raii.c
+++ src/raii.c
@@ -37,31 +37,6 @@ int gettimeofday(struct timeval *tp, struct timezone *tzp) {
     tp->tv_usec = (long)(system_time.wMilliseconds * 1000);
     return 0;
 }
-
-#ifndef HAVE_GETENTROPY
-int getentropy(void *buf, size_t len) {
-	HCRYPTPROV provider;
-
-	if (len > 256) {
-		errno = EIO;
-		return (-1);
-	}
-
-	if (CryptAcquireContext(&provider, NULL, NULL, PROV_RSA_FULL,
-		CRYPT_VERIFYCONTEXT) == 0)
-		goto fail;
-	if (CryptGenRandom(provider, len, buf) == 0) {
-		CryptReleaseContext(provider, 0);
-		goto fail;
-	}
-	CryptReleaseContext(provider, 0);
-	return (0);
-
-fail:
-	errno = EIO;
-	return (-1);
-}
-#endif
 #endif
 
 int cerr(string_t msg, ...) {
